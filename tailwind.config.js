842dbda4ddf47a491791811f1d825c006ac855a9
Cristina <cristina.carretero@aytecdigital.com>
2022-10-06 14:07:29 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+        screens: {
+            'xxs': '320px',
+            'xs': '480px',
+            ...defaultTheme.screens,
+        },
+        extend: {
+            fontFamily: {
+                'barlow': ['Barlow', 'sans-serif'],
+                'inter': ['Inter', 'sans-serif']
+            },
+            colors: {
+                ligth: '#E5FAFA',
+                primary: '#00D0CD',
+                secondary: '#000563',
+                red: '#B33A3A',
+                cancel: '#D95F52',
+                orange: '#FFC061',
+                grey: '#29546B',
+            }
+        },
+    },
+    plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')],
+};
